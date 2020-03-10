78a3e37052b763103ab5e460df7b16027528d204
Warwick Booth <warwick@lsdev.biz>
2020-03-10 09:37:45 +0200

    Fixed - using the development version of react #45

--- src/blocks.js
+++ src/blocks.js
@@ -13,6 +13,7 @@ import './blocks/block-card/index.js';
 import './blocks/block-banner/index.js';
 import './blocks/block-post-carousel/index.js';
 import './blocks/block-team/index.js';
+import './blocks/block-testimonials/index.js';
 
 //Extensions
 import './extensions/block-image/spacing-control.js';
@@ -21,6 +22,3 @@ import './extensions/block-button/background-control.js';
 import './extensions/block-group/group-control.js';
 import './extensions/block-heading/heading-control.js';
 import './extensions/block-cover/cover-control.js';
-
-//Testimonial Block
-import './blocks/block-testimonials/index.js';
