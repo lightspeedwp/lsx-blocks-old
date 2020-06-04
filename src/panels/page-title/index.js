643bd03fd2a8f7c141f9d7cfb13eb00bd510d7f1
Warwick Booth <warwick@lsdev.biz>
2020-06-04 07:53:32 +0200

    Restricting the page title to posts and pages to test the code.

--- src/panels/page-title/index.js
+++ src/panels/page-title/index.js
@@ -11,10 +11,12 @@ import DisableTitle from './components/disable-title';
 import MyDropdown from './components/drop-down';
 
 const lsxPageTitlePanel = () => {
-	/*const postType = useSelect( select => select( 'core/editor' ).getCurrentPostType() );
-	if ( 'post' !== postType ) {
+
+	// Here we check what the post type is and if your panel should show.
+	const postType = useSelect( select => select( 'core/editor' ).getCurrentPostType() );
+	if ( 'post' !== postType && 'page' !== postType ) {
 		return null;
-	}*/
+	}
 
 	return (
 		<PluginDocumentSettingPanel
